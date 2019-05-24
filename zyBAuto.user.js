8a418caa06e92bbcf7943b2d6554be2beff29d30
Evan G <yopu1234@gmail.com>
2019-05-24 14:44:37 -0700

    A little refactoring

--- zyBAuto.user.js
+++ zyBAuto.user.js
@@ -7,45 +7,64 @@
 // @namespace https://github.com/Evanito/zyBAuto
 // @run-at document-idle
 // ==/UserScript==
-// TO USE:  <-----
+// TO USE: Click Autocomplete! on a zyBooks page <-----
+
+// ==== SETTINGS ====
+var autoRun = false;
+// == END SETTINGS ==
+
 // Do not edit below this line!
 // ==========================================
 (function() {
     console.log(timeString() + " [zBA] Begin zyBooks Autocomplete by Evanito.");
-    document.getElementsByClassName('right-buttons')[0].innerHTML += '<button id="zbaButton" type="button">Autocomplete!</button>';
-    document.getElementById("zbaButton").addEventListener ("click", ButtonClickAction, false);
+    if (autoRun) {
+        run();
+    } else {
+        document.getElementsByClassName('right-buttons')[0].innerHTML += '<button id="zbaButton" type="button">Autocomplete!</button>';
+        document.getElementById("zbaButton").addEventListener ("click", zBAStartButton, false);
+    }
 })();
 
 
-function ButtonClickAction (zEvent) {
+function zBAStartButton (zEvent) {
     console.log(timeString() + " [zBA] Running...");
     run();
 }
 
 function run() {
+    //click_speeds();
+    click_plays();
+    click_starts();
+    setTimeout(function(){ run(); }, 1000);
+}
 
-    var i;
+function click_speeds() { // Checks speed boxes. Doesn't work but isn't a necessary feature.
     var speed = document.getElementsByClassName("speed-control");
-    for (i = 0; i < speed.length; i++) {
+    for (var i = 0; i < speed.length; i++) {
         if ((speed[i].innerHTML).includes("false")) {
             //speed[i].click();
             speed[i].getElementsByClassName("zb-checkbox")[0].innerHTML = "\n<input type=\"checkbox\" value=\"true\" aria-label=\"2x speed\">\n<label aria-hidden=\"true\">2x speed</label>\n"
             console.log(timeString() + " Checked a speed box.");
         }
     }
+}
+
+function click_plays() { // Clicks all Play buttons
     var plays = document.getElementsByClassName("play-button");
-    for (i = 0; i < plays.length; i++) {
+    for (var i = 0; i < plays.length; i++) {
         if (!(plays[i].classList).contains("rotate-180")){
             plays[i].click();
             console.log(timeString() + " Clicked a play button.");
         }
     }
+}
+
+function click_starts() { // Clicks all Start buttons
     var starts = document.getElementsByClassName("start-button");
-    for (i = 0; i < starts.length; i++) {
+    for (var i = 0; i < starts.length; i++) {
         starts[i].click();
         console.log(timeString() + " Clicked a start button.");
     }
-    setTimeout(function(){ run(); }, 1000);
 }
 
 function timeString() {
